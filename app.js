2057380f6ff1529cfd864f4ef5d8cc58f38cd028
senamrutinpgsdumuslim-cmd <senamrutinpgsdumuslim@gmail.com>
2026-08-05 02:10:46 +0700

    Update app.js

--- app.js
+++ app.js
@@ -4,7 +4,7 @@
 
 let MODE = "";
 
-const URL_APPS_SCRIPT = "https://script.google.com/macros/s/AKfycbzwBgCzvU7H1LSMEiVO8gJ9iy1g1EmXhUchuewpBMQiJgIiWq3IJIgH8Y6H5m0nrU_3rw/exec";
+const URL_APPS_SCRIPT = "https://script.google.com/macros/s/AKfycbzwBgCzvU7H1LSMEiVO8gJ9iy1g1EmXhUchuewpBMQiJgIiWq3IJIgH8Y6H5m0nrU_3rw/exec?api=dashboard";
 
 const hasil = document.getElementById("hasil");
 const popup = document.getElementById("popup");
